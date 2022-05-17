811f8906ebea6b5b21c791673c4efcbf7f480f95
daijiong <djflying1988@163.com>
2022-05-17 21:44:49 +0800

    test webhook

--- src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
+++ src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class XxlJobExecutorApplication {
-
 	public static void main(String[] args) {
         SpringApplication.run(XxlJobExecutorApplication.class, args);
 	}
